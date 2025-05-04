8a794a25787de5469432aa9a056e872af05acd4f
FadilRifqi <fadilataullahrifqi@gmail.com>
2025-05-04 22:18:18 +0700

    update entry point

--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,37 @@
+import React, { useEffect, useState } from 'react';
 import { Redirect } from 'expo-router';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Index() {
-  return <Redirect href="/screens/Splash/SplashScreen" />;
+  const [initialRoute, setInitialRoute] = useState(null);
+
+  useEffect(() => {
+    const checkUserData = async () => {
+      try {
+        const userData = await AsyncStorage.getItem('userData');
+        if (userData) {
+          // Jika userData ada, arahkan ke halaman Home
+          setInitialRoute({
+            href: '/screens/HomeScreen',
+            data: JSON.parse(userData),
+          });
+        } else {
+          // Jika tidak ada userData, arahkan ke halaman SplashScreen
+          setInitialRoute({ href: '/screens/Splash/SplashScreen' });
+        }
+      } catch (error) {
+        console.error('Error checking userData:', error);
+        setInitialRoute({ href: '/screens/Splash/SplashScreen' });
+      }
+    };
+
+    checkUserData();
+  }, []);
+
+  if (!initialRoute) {
+    // Tampilkan layar kosong atau loader sementara menunggu pengecekan
+    return null;
+  }
+
+  return <Redirect href={initialRoute.href} />;
 }
